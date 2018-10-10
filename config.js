f7031dceee33dd555e83b5853937eabc04cc30d4
Suswith Gaddam <suswithgaddam@Suswiths-MacBook-Pro.local>
2018-10-10 16:20:42 -0400

    made the API_Key constant

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
 //API Key
 
-API_Key = "pk.eyJ1Ijoic3VzaHdpdGg1NDciLCJhIjoiY2psY3gya2tlMDU4aDNwczV3aWYyMWl5OSJ9.X7jcUimk0RMP8gLCLPx24A";
+const API_Key = "pk.eyJ1Ijoic3VzaHdpdGg1NDciLCJhIjoiY2psY3gya2tlMDU4aDNwczV3aWYyMWl5OSJ9.X7jcUimk0RMP8gLCLPx24A";
